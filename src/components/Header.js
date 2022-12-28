f9fbd9f8576baaff38dc347ec0f50c37092d582f
AlliSuresh-ZLR <suresh.a@zelarsoft.com>
2022-12-28 14:58:49 +0530

    loading spinner added while fetching the data

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,6 +10,7 @@ import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
 import axios from 'axios';
 import Moviecard from './Moviecard';
+import CircularProgress from '@mui/material/CircularProgress';
 import './moviecard.css'
 
 
@@ -61,6 +62,7 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
 export default function Header() {
   const [searchkey, setSearchkey] = React.useState("");
   const [data, setData] = React.useState([]);
+  const [loading,setLoading]=React.useState(false)
 
   
 
@@ -76,10 +78,11 @@ export default function Header() {
       }
     };
     if (e.key === "Enter") {
-
+setLoading(true)
       await axios.request(options).then(function (response) {
         console.log(response.data);
         setData(response.data.d);
+        setLoading(false)
 
       }).catch(function (error) {
         console.error(error);
@@ -125,9 +128,8 @@ export default function Header() {
       </AppBar>
 
       <div className={data.length?"moviecontainer":"emptycontainer"} >
-        {}
 
-        {data.map((item,index) => <Moviecard key={index}title={item.l} url={item.i?item.i.imageUrl:"https://www.nbu.ac.in/img/dept/anthropology/slider/slider3.jpg"} />)}
+        {!loading?data.map((item,index) => <Moviecard key={index}title={item.l} url={item.i?item.i.imageUrl:"https://www.nbu.ac.in/img/dept/anthropology/slider/slider3.jpg"} />):<CircularProgress/>}
       
       </div>
 
